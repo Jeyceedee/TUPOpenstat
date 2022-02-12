a33d02adf9bfd38dc94b17bf220f09ba8cee0e29
arvinlaya <89141312+arvinlaya@users.noreply.github.com>
2022-02-12 11:17:11 +0800

    login fix

--- application/views/templates/login.php
+++ application/views/templates/login.php
@@ -2,7 +2,7 @@
     <button id="closeSignIn">&#10005;</button>
     <form method="post" action="<?= base_url("login/") ?>">
         <h2>SIGN IN</h2>
-        <label for="loginEmail">Email</label><br>
+        <label for="loginEmail">Email</label>
         <input id="loginEmail" type="email" name="loginEmail" required><br>
         <label for="loginPassword">Password</label>
         <input id="loginPassword" type="password" name="loginPassword" required>
